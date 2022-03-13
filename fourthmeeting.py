1683da817534ce73197ca6030d3d16c8ee37f278
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-03-13 17:19:53 +0000

    First PCC UCC python codes

---
+++ fourthmeeting.py
@@ -0,0 +1,22 @@
+# # print("Ama" + " is going to school")
+#
+# # print("Peter"*1000)
+#
+# # print(abs(-46))
+#
+#
+#
+# print(len("Nyamekesse is good guy"))
+#
+# print('What is your name')
+#
+# name = input()
+#
+# print('Now printing the next line of code')
+
+
+print("Where do you come from")
+
+location = input()
+
+print("You typed " + location)
---
+++ hello.py
@@ -0,0 +1,10 @@
+
+
+# myCGPA = 2**2
+div = 5//2
+div2 = 10/2
+mod1 = 3 % 2
+
+# print(div)
+# print(div2)
+print(mod1)
