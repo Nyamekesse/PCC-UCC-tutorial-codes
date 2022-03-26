288519115a9560097424095a606f8e6ce09c176e
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-03-26 17:07:16 +0000

    Added a new file

---
+++ Strings.py
@@ -0,0 +1,36 @@
+# myGreetings = 'How\'s your day going?'
+#
+# reportingASpeech = "He said \"Shut up and sit down\""
+#
+# a = r'''He said "Shutup and sit down" '''
+#
+# b = "This is a sentence with a tab"
+
+# print(b.index("s"))
+# print(b.index('s',5, 9))
+# print(b.index('s'))
+
+# print("Jessica" not in b)
+
+a = "Python"
+# a = a.upper()
+# print(a)
+# print(a.upper())
+# print(a)
+
+# print(a.isupper())
+
+# c = "147\u2070"
+
+# d = "Hello,world"
+# print(d.startswith("e"))
+
+# myWishList = ['Car', '5.9', 'mansion']
+
+name = "Kwame Nkansah Ntim"
+newName = name.split(' ')
+print(' '.join(newName))
+
+# print(newName)
+
+
