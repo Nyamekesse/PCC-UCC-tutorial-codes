bb1468cb93b32a5c83dff94754a89817e6f7c11d
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-03-27 19:05:55 +0100

    updated files

---
+++ string_methods.py
@@ -0,0 +1,13 @@
+# testType = "123abdck"
+
+# print(testType.isdigit())
+
+# validator = "Mrs. Mary Attipoe"
+# print(validator.endswith("Attipoe"))
+
+# validator = ["i", "am", "happy"]
+
+# print('/'.join(validator))
+# sentence = "My name is PHOENIX"
+# print(sentence.split())
+
